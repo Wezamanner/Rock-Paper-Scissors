9095c24e4f6c5a97e34ae0bade487db8df383a26
Wezamanner <wezirmuhammed1184@gmail.com>
2026-03-01 12:26:13 +0300

    Updated UI logic

--- script.js
+++ script.js
@@ -8,59 +8,110 @@ if(preComputerChoice==1) computerChoice = `paper`;
 if(preComputerChoice==2) computerChoice = `rock`;
 return computerChoice
 }
-// Get the user choice by using prompt and saving it to a variable
-function getUserChoice(){
-   let userChoice = prompt(`Choose Between Rock-Paper-scissors😒`, );
-   return userChoice;
-}
+
+// Get the user choice by click event listener
+const div =document.querySelector(`div`)
+
+const BTN1 = document.createElement(`button`)
+BTN1.textContent = `Rock`;
+div.appendChild(BTN1)
+const BTN2 = document.createElement(`button`);
+BTN2.textContent = `Paper`;
+div.appendChild(BTN2)
+const BTN3 = document.createElement(`button`);
+BTN3.textContent = `Scissors`;
+div.appendChild(BTN3)
+const resultDev= document.createElement(`div`);
+div.appendChild(resultDev);
+const score = document.createElement(`div`);
+div.appendChild(score);
+const winnerAnouncer = document.createElement(`div`);
+div.appendChild(winnerAnouncer)
+BTN1.addEventListener(`click` , () =>{
+   let hChoice= BTN1.textContent.toLocaleLowerCase();
+   const computerSelection = getComputerChoice();
+   playRound(hChoice,computerSelection);
+
+});
+BTN2.addEventListener(`click`, ()=>{
+    const computerSelection = getComputerChoice();
+    let hChoice = BTN2.textContent.toLowerCase();
+    playRound(hChoice,computerSelection);
+    
+});
+BTN3.addEventListener(`click`,()=>{
+    const computerSelection = getComputerChoice();
+  let hChoice = BTN3.textContent.toLocaleLowerCase();
+    playRound(hChoice,computerSelection);
+    
+} )
+// function getUserChoice(){
+//    let userChoice = prompt(`Choose Between Rock-Paper-scissors😒`, );
+//    return userChoice;
+// }
 // declaring scores to save the score of the human and computer
 let humanScore = 0;
 let computerScore = 0;
 // The logic of the single round of the game is done here within this function
-function playRound (humanChoice , computerChoice){
-    if (humanChoice==`rock` && computerChoice == `scissors`)
-return `you won!`
-	else if(humanChoice == `scissors` && computerChoice ==`rock`)
-		return `you lose!`
-else if (humanChoice ==`paper` && computerChoice == `rock`)
-return `you won!`
-else if (humanChoice==`rock` && computerChoice == `paper`)
-return `you lose!`
-else if (humanChoice == `paper` && computerChoice == `scissors`)
-return  `you lose!`
-else if (humanChoice == `scissors` && computerChoice ==`paper`)
-return `you won!`
-else if (humanChoice==computerChoice)
-    return `play again!`;
+function playRound (humanChoice, computerChoice){
+    if (humanChoice==`rock` && computerChoice == `scissors`||humanChoice ==`paper` && computerChoice == `rock`||humanChoice == `scissors` && computerChoice ==`paper`){
+        humanScore++;
+
+resultDev.textContent=`you won!`}
+	else if(humanChoice == `scissors` && computerChoice ==`rock`||humanChoice==`rock` && computerChoice == `paper`||humanChoice == `paper` && computerChoice == `scissors`){
+        computerScore++;
+		resultDev.textContent= `you lose!`}
+
+    else if (humanChoice==computerChoice)
+   resultDev.textContent= `play again!`;
+console.log(computerScore,humanScore);
+
+score.textContent=`You Scored${humanScore} and the computer scored ${computerScore}`
+if(computerScore==5&&humanScore<5){
+    score.textContent=`SORRY😔 YOU LOSE!!!`;
 }
+if(humanScore==5&&computerScore<5){
+   score.textContentn=`HUREY YO WON THE MATCH🏆!!!`
+}
+if(humanScore>5||computerScore>5){
+  score.textContent==`the Game has already ended`;
+}
+
+
+}
+// div.style = `display: flex; justify-content: center; font-style:Segoe UI , sans-serif; gap: 5rem;margin-top: 20rem ; flex-direction: column`;
+
+BTN1.style= `padding :30px; margin: 10px ; background-color:red ; border-radius: 20px;`;
+BTN2.style =`padding :30px; margin: 10px ; background-color:red ; border-radius: 20px`;
+BTN3.style =`padding :30px; margin: 10px ; background-color:red ; border-radius: 20px`;
+
 // the game logic for the score keeping is in this function 
-function playGame(){
+// function playGame(Event){
+
+// const computerSelection = getComputerChoice();
+
+
+//     let round = playRound ( humanSelection ,computerSelection );
+
+// if (round ==`you won!`){
+//     humanScore++;
     
-const humanSelection = getUserChoice();
-const computerSelection = getComputerChoice();
+// }
+// else if (round==`you lose!`){
+//     computerScore++;
+// }
+// else if(round==`play again!`){
+//     humanScore+=0;
+//     computerScore+=0;
+// }
 
-
-    let round = playRound ( humanSelection ,computerSelection );
-
-if (round ==`you won!`){
-    humanScore++;
-    
-}
-else if (round==`you lose!`){
-    compuerScore++;
-}
-else if(round==`play again!`){
-    humanScore+=0;
-    computerScore+=0;
-}
-
-return round;
-}
+// return round;
+// }
 // playing the game 5 rounds using the for loop
-for(let i =0; i<5;i++){
+// for(let i =0; i<5;i++){
 
-   console.log(playGame()); 
-}
+//    console.log(playGame()); 
+// }
 // Declaring the winner after 5 ROUNDS
 if(humanScore>computerScore){
        console.log(`YOU ARE THE WINNER BY ${humanScore} TO ${computerScore}`);
